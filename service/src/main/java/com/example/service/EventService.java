eefb868b5319550ab80bac11df7872918a53d88d
omar <omarkhaledmaher@gmail.com>
2026-05-10 16:44:30 +0300

    Refactored createEvent method to handle DatabaseLoggingEvent

--- service/src/main/java/com/example/service/EventService.java
+++ service/src/main/java/com/example/service/EventService.java
@@ -1,19 +1,24 @@
 package com.example.service;
 
 import java.util.List;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.example.common.events.DatabaseLoggingEvent;
 import com.example.common.exceptions.ResourceNotFoundException;
 import com.example.model.Event;
 import com.example.repository.EventRepository;
+import com.example.service.mapper.EventMapper;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
+    private final EventMapper mapper;
 
     public Event getEvent(Long id) {
         return eventRepository.findById(id)
@@ -26,8 +31,11 @@ public class EventService {
     }
 
     @Transactional
-    public Event createEvent(Event event) {
-        Event createdEvent = eventRepository.save(event);
+    @EventListener
+    public Event createEvent(DatabaseLoggingEvent databaseLoggingEvent) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Event createdEvent = eventRepository.save(mapper.toEvent(databaseLoggingEvent, username));
         eventRepository.flush();
         return createdEvent;
     }
