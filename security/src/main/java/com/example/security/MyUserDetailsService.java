b5c320f1f44ceb76f17b4e4de86f76f6c59e568b
omar <omarkhaledmaher@gmail.com>
2026-05-10 12:16:07 +0300

    Added AuthTokenFilter and custom UserDetailsService implementation for JWT authentication

---
+++ security/src/main/java/com/example/security/MyUserDetailsService.java
@@ -0,0 +1,68 @@
+package com.example.security;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.model.Privilege;
+import com.example.model.Role;
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).map(toUserDetails())
+                .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
+    }
+
+    private List<? extends GrantedAuthority> getAuthorities(List<Role> roles) {
+        return getGrantedAuthorities(getPrivileges(roles));
+    }
+
+    private List<String> getPrivileges(List<Role> roles) {
+
+        var privileges = new ArrayList<String>();
+        var collection = new ArrayList<Privilege>();
+        for (Role role : roles) {
+            privileges.add("ROLE_" + role.getName().toString());
+            collection.addAll(role.getPrivileges());
+        }
+        for (Privilege item : collection) {
+            privileges.add(item.getName());
+        }
+        return privileges;
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+        var authorities = new ArrayList<GrantedAuthority>();
+        for (String privilege : privileges) {
+            authorities.add(new SimpleGrantedAuthority(privilege));
+        }
+        return authorities;
+    }
+
+    private Function<User, UserDetails> toUserDetails() {
+        return user -> new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                getAuthorities(user.getRoles()));
+    }
+}
