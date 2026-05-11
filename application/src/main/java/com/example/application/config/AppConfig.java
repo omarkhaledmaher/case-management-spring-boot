9874e5b2a80899a2b93d240a839da7f1d278ee41
omar <omarkhaledmaher@gmail.com>
2026-05-11 10:20:41 +0300

    Added ActiveMQ configuration and dependencies

---
+++ application/src/main/java/com/example/application/config/AppConfig.java
@@ -0,0 +1,32 @@
+package com.example.application.config;
+
+import org.springframework.boot.jms.autoconfigure.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.converter.JacksonJsonMessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import jakarta.jms.ConnectionFactory;
+
+@Configuration
+@EnableJms
+public class AppConfig {
+    @Bean
+    DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory,
+            DefaultJmsListenerContainerFactoryConfigurer configurer) {
+
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
+
+    @Bean
+    MessageConverter jacksonJmsMessageConverter() {
+        JacksonJsonMessageConverter converter = new JacksonJsonMessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
