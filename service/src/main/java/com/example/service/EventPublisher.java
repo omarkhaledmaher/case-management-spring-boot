3e5160adae02ea76b7d6a7feabc7dc90f2aab000
omar <omarkhaledmaher@gmail.com>
2026-05-18 10:59:06 +0300

    Added second publishEventMethod which doesnt have username as parameter

--- service/src/main/java/com/example/service/EventPublisher.java
+++ service/src/main/java/com/example/service/EventPublisher.java
@@ -1,15 +1,23 @@
 package com.example.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 import com.example.common.dto.EventDto;
 import com.example.common.enums.DatabaseOperation;
+import com.example.security.IAuthFacade;
+import lombok.AllArgsConstructor;
 
 @Component
+@AllArgsConstructor
 public class EventPublisher {
-    @Autowired
-    private JmsTemplate jmsTemplate;
+    private final IAuthFacade authFacade;
+    private final JmsTemplate jmsTemplate;
+
+    public void publishEvent(DatabaseOperation operation, String entityName, String methodName, Object result) {
+        String username = authFacade.getAuthentication().getName();
+        jmsTemplate.convertAndSend("database.logging",
+                new EventDto(operation, entityName, methodName, username, result.toString()));
+    }
 
     public void publishEvent(DatabaseOperation operation, String entityName, String methodName, String username,
             Object result) {
