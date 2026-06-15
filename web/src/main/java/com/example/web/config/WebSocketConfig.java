0ce418ba515b28f3d64bd6cb439776537479ce07
omar <omarkhaledmaher@gmail.com>
2026-06-15 10:22:15 +0300

    Added localhost:4200 as an allowed origin in web socket config

--- web/src/main/java/com/example/web/config/WebSocketConfig.java
+++ web/src/main/java/com/example/web/config/WebSocketConfig.java
@@ -24,8 +24,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("http://localhost:4200");
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("http://localhost:4200").withSockJS();
     }
 
     @Override
