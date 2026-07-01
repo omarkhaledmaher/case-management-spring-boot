824f2df6a3916912b99b4990dd30a3afc5119e75
omar <omarkhaledmaher@gmail.com>
2026-07-01 13:22:30 +0300

    Changed roles and privileges to be eagerly fetched when getting user by username

--- repository/src/main/java/com/example/repository/UserRepository.java
+++ repository/src/main/java/com/example/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.example.repository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"roles", "roles.privileges"})
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
